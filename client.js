a0741dc992dd2858934e880d21353aba7f76af59
kip2 <tqa1no4@gmail.com>
2023-08-31 11:59:58 +0900

    nodejsによる標準入力の受付テスト

--- client.js
+++ client.js
@@ -1,31 +1,20 @@
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
-const http = require('http');
+const webclient = require('request');
 
-const data = JSON.stringify({
-  "method": "floor",
-  "params": [13.5],
-  "jsonrpc": "2.0",
-  "id": 0,
-});
 
-const content = "content"
-
-const options = {
-  method: "POST",
+webclient.post({
+  url: "http://127.0.0.1:4000/jsonrpc",
   headers: {
-    "Content-Type": "application/json",
-    'Content-Length': '' + content.length,
-    'X-Header': 'X-Header',
+    "content-type": "application/json"
   },
-};
-const url = "http://127.0.0.1:4000/jsonrpc"
-//const url = "http://localhost:4000/jsonrpc"
-const request = http.request(url, options, response => {
-  console.log(`statusCode: ${response.statusCode}`)
-})
-console.log(request);
-request.write(data);
-request.end();
-
+  body: JSON.stringify({
+    "method": "floor",
+    "params": [13.5],
+    "jsonrpc": "2.0",
+    "id": 0,
+  })
+}, function (error, response, body){
+  console.log(body);
+});
