3c382eb1e6a6a06bad4a539b677f56dff311863f
kip2 <tqa1no4@gmail.com>
2023-08-31 15:43:21 +0900

    client.js完成

--- client.js
+++ client.js
@@ -4,20 +4,20 @@ const require = createRequire(import.meta.url);
 const webclient = require('request');
 const fsPromises = require('fs/promises');
 
-export async function clientRequest() {
+export async function clientRequest(command, args) {
   const answer = await webclient.post({
     url: "http://127.0.0.1:4000/jsonrpc",
     headers: {
       "content-type": "application/json"
     },
     body: JSON.stringify({
-      "method": "floor",
-      "params": [13.5],
+      "method": command,
+      "params": args,
       "jsonrpc": "2.0",
       "id": 0,
     })
   }, function (error, response, body){
+    const jsonObj = JSON.parse(body);
+    return console.log(jsonObj['result']);
   });
-
-  console.log("client:", answer.body);
 }
