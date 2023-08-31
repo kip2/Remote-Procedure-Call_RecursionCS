d1c55b9923a8f2a6ce838a69395e00852ced687a
kip2 <tqa1no4@gmail.com>
2023-08-31 14:26:40 +0900

    簡単なREPL完成

--- client.js
+++ client.js
@@ -2,19 +2,22 @@ import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
 const webclient = require('request');
+const fsPromises = require('fs/promises');
 
+export async function clientRequest() {
+  const answer = await webclient.post({
+    url: "http://127.0.0.1:4000/jsonrpc",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      "method": "floor",
+      "params": [13.5],
+      "jsonrpc": "2.0",
+      "id": 0,
+    })
+  }, function (error, response, body){
+  });
 
-webclient.post({
-  url: "http://127.0.0.1:4000/jsonrpc",
-  headers: {
-    "content-type": "application/json"
-  },
-  body: JSON.stringify({
-    "method": "floor",
-    "params": [13.5],
-    "jsonrpc": "2.0",
-    "id": 0,
-  })
-}, function (error, response, body){
-  console.log(body);
-});
+  console.log("client:", answer.body);
+}
